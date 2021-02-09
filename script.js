7695e14905a93fd3a4720d47a43135abfa89d090
kristinmerbach <kristin@kristinmerbach.com>
2021-02-09 14:00:33 -0500

    added contact form

--- script.js
+++ script.js
@@ -71,3 +71,58 @@ document.addEventListener("mousemove", (e)=> {
 		reye.style.cx = `${rhM}`;
 	}
 });
+
+//form
+window.addEventListener("DOMContentLoaded", function() {
+
+  // get the form elements defined in your form HTML above
+  
+  var form = document.getElementById("my-form");
+  var button = document.getElementById("my-form-button");
+  var status = document.getElementById("my-form-status");
+
+  // Success and Error functions for after the form is submitted
+  
+  function success() {
+    form.reset();
+    form.style = "display: none ";
+    status.style = "display: block;"
+  }
+
+  function error() {
+    status.innerHTML = "Oops! There was a problem, try emailing my at kristin@kristinmerbach.com.";
+  }
+
+  // handle the form submission event
+
+  form.addEventListener("submit", function(ev) {
+    ev.preventDefault();
+    var data = new FormData(form);
+    ajax(form.method, form.action, data, success, error);
+  });
+});
+
+//form re-reveal
+function revealForm() {
+  var form = document.getElementById("my-form");
+  var status = document.getElementById("my-form-status");
+    status.style = "display: none ";
+    form.style = "display: block;"
+  }
+
+// helper function for sending an AJAX request
+
+function ajax(method, url, data, success, error) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.setRequestHeader("Accept", "application/json");
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState !== XMLHttpRequest.DONE) return;
+    if (xhr.status === 200) {
+      success(xhr.response, xhr.responseType);
+    } else {
+      error(xhr.status, xhr.response, xhr.responseType);
+    }
+  };
+  xhr.send(data);
+}
